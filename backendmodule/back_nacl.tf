7216fd62c156c5d284716797ecc7ceba934ba350
Bhaskar Roy <broy@ibm.com>
2021-08-28 16:17:40 -0400

    Data Governance and Backend got net acl templates switched previously, fixed in this commit

---
+++ backendmodule/back_nacl.tf
@@ -0,0 +1,10 @@
+
+##############################################################################
+# Config to dynamically create data governance host Network ACL and rules
+#
+# Base rules for access to DNS, repos are predefined. Inputs required for 
+# source subnets data governance VSIs host will be connected to by the front end VSIs and 
+# bastion host 
+##############################################################################
+
+# placeholder
---
+++ datgovmodule/data_nacl.tf
@@ -0,0 +1,10 @@
+
+##############################################################################
+# Config to dynamically create backend host Network ACL and rules
+#
+# Base rules for access to DNS, repos are predefined. Inputs required for 
+# source subnets backend VSIs host will be connected to by the front end VSIs and 
+# bastion host 
+##############################################################################
+
+# placeholder
