6cfe4f518d902df4574bb38c7c28a164947ef612
Bhaskar Roy <broy@ibm.com>
2021-08-28 12:09:10 -0400

    Added version to every module, the should be linked to root, if needed, as all modules may or may not use the same provider version

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-
 # provider block required with Schematics to set VPC region
 provider "ibm" {
   region = var.ibm_region
   #ibmcloud_api_key = var.ibmcloud_api_key
   generation = local.generation
-  version    = "~> 1.4"
+  # version    = "~> 1.4"
 }
 
 data "ibm_resource_group" "all_rg" {
